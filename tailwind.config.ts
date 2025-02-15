9667c9ebeea51985f840beeed8d644f701a61fe9
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-02-15 13:55:51 -0700

    started adding some custom styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,10 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["var(--geist-font)", "Inter", "system-ui"],
+        brand: ["var(--gabarito-font)"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
