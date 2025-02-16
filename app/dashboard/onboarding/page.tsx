9679386f414b949b4874cd25db7d7d323c1ca57c
Burton Jong <108391733+burtonjong@users.noreply.github.com>
2025-02-16 01:09:21 -0700

    test (#25)
    * test
    
    * test2

--- app/dashboard/onboarding/page.tsx
+++ app/dashboard/onboarding/page.tsx
@@ -1,42 +1,11 @@
-"use client";
+import { Suspense } from "react";
 
-import React, { useState } from "react";
+import Onboarding from "@/components/onboarding/onboarding";
 
-export default function Onboarding() {
-  const [teachSkills, setTeachSkills] = useState("");
-  const [learnSkills, setLearnSkills] = useState("");
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // Handle form submission logic here
-    console.log("Skills to teach:", teachSkills);
-    console.log("Skills to learn:", learnSkills);
-  };
-
+export default async function Page() {
   return (
-    <div className="onboarding-container">
-      <h1>Welcome to the Skills Exchange Platform!</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="teachSkills">Skills you can teach:</label>
-          <input
-            type="text"
-            id="teachSkills"
-            value={teachSkills}
-            onChange={(e) => setTeachSkills(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="learnSkills">Skills you want to learn:</label>
-          <input
-            type="text"
-            id="learnSkills"
-            value={learnSkills}
-            onChange={(e) => setLearnSkills(e.target.value)}
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <Suspense>
+      <Onboarding />;
+    </Suspense>
   );
 }
