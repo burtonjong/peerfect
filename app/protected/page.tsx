808dd3525be1c68353f462898fc23d709d7feeab
Matthew Hrehirchuk <matthew_hre@outlook.com>
2025-02-15 22:31:55 -0700

    added some shitty dashboards to test chat

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -15,20 +15,83 @@ export default async function ProtectedPage() {
     return redirect("/sign-in");
   }
 
+  const getUserData = async () => {
+    const { data, error } = await supabase
+      .from("user_profiles")
+      .select("*")
+      .eq("id", user.id)
+      .single();
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    return data;
+  };
+
+  const getAllPosts = async () => {
+    const { data, error } = await supabase.from("posts").select("*");
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    return data;
+  };
+
+  const getAllConversations = async () => {
+    const { data, error } = await supabase
+      .from("conversations")
+      .select("*, post_id:posts(id,title)")
+      .or(`poster_id.eq.${user.id},responder_id.eq.${user.id}`);
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    console.log(data);
+
+    return data;
+  };
+
+  const userData = await getUserData();
+  const posts = await getAllPosts();
+  const conversations = await getAllConversations();
+
   return (
     <div className="flex w-full flex-1 flex-col gap-12">
-      <div className="w-full">
-        <div className="flex items-center gap-3 rounded-md bg-accent p-3 px-5 text-sm text-foreground">
-          <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
-          user
-        </div>
+      <div className="flex items-center gap-4">
+        <InfoIcon size={24} />
+        <h1 className="text-2xl">Protected Page</h1>
       </div>
-      <div className="flex flex-col items-start gap-2">
-        <h2 className="mb-4 text-2xl font-bold">Your user details</h2>
-        <pre className="max-h-32 overflow-auto rounded border p-3 font-mono text-xs">
-          {JSON.stringify(user, null, 2)}
-        </pre>
+      <div>
+        <h2 className="text-xl">User Data</h2>
+        <pre>{JSON.stringify(userData, null, 2)}</pre>
+      </div>
+      <div>
+        <h2 className="text-xl">Posts</h2>
+        <ul>
+          {posts?.map((post) => (
+            <li key={post.id}>
+              <a href={`/protected/${post.id}`}>{post.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2 className="text-xl">Conversations</h2>
+        <ul>
+          {conversations?.map((conversation) => (
+            <li key={conversation.id}>
+              <a href={`/protected/${conversation.post_id.id}/chat`}>
+                {conversation.post_id.title} chat
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
