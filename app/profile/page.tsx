ee08b1d97c757cfc5bae9aaf1bc4620f52b2586c
Burton Jong <108391733+burtonjong@users.noreply.github.com>
2025-02-15 22:38:39 -0700

    Profile connect (#19)
    * for burty
    
    * done profile
    
    ---------
    
    Co-authored-by: anthonyych4n <anthony168c@gmail.com>

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,65 +1,27 @@
-"use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-
 import { ProfileContent } from "@/components/profile/profile-content";
 import { ProfileHeader } from "@/components/profile/profile-header";
 import { Card } from "@/components/ui/card";
-import { Toaster } from "@/components/ui/toaster";
+import { createClient } from "@/utils/supabase/server";
 
-const profileFormSchema = z.object({
-  firstName: z.string().min(2, {
-    message: "First name must be at least 2 characters.",
-  }),
-  lastName: z.string().min(2, {
-    message: "Last name must be at least 2 characters.",
-  }),
-  age: z.number().min(18, {
-    message: "You must be at least 18 years old.",
-  }),
-  bio: z.string().max(500, {
-    message: "Bio must not be longer than 500 characters.",
-  }),
-  skillsGoodAt: z.array(z.string()),
-  skillsNeedHelpWith: z.array(z.string()),
-  isAvailableToHelp: z.boolean(),
-  isLookingForHelp: z.boolean(),
-});
+export default async function ProfilePage() {
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-type ProfileFormValues = z.infer<typeof profileFormSchema>;
-
-const defaultValues: Partial<ProfileFormValues> = {
-  firstName: "",
-  lastName: "",
-  age: 18,
-  bio: "",
-  skillsGoodAt: [],
-  skillsNeedHelpWith: [],
-  isAvailableToHelp: false,
-  isLookingForHelp: false,
-};
-
-export default function ProfilePage() {
-  const form = useForm<ProfileFormValues>({
-    resolver: zodResolver(profileFormSchema),
-    defaultValues,
+  const { data: enums } = await supabase.rpc("get_types", {
+    enum_type: "skill_enum",
   });
 
-  function onSubmit(data: ProfileFormValues) {
-    Toaster({
-      title: "Profile updated",
-      description: "Your profile has been successfully updated.",
-    });
-    console.log(data);
+  if (!user) {
+    return <div>Failed to load user data</div>;
   }
 
   return (
     <div className="container mx-auto py-10">
       <Card>
-        <ProfileHeader />
-        <ProfileContent form={form} onSubmit={onSubmit} />
+        <ProfileHeader user={user} />
+        <ProfileContent user={user} enums={enums} />  
       </Card>
     </div>
   );
